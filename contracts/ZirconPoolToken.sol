d849bd08cf97b74505b4bab635717c108232b707
David Vittori <me@vittoridavid.com>
2021-12-12 18:04:20 +0100

    ZA-5 pool token boilerplate

--- contracts/ZirconPoolToken.sol
+++ contracts/ZirconPoolToken.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.6.2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./zircon-uniswapv2/interfaces/IUniswapV2ERC20.sol";
-contract ZirconAnchor is IUniswapV2ERC20, Ownable {
+
+contract ZirconAnchor is IUniswapV2ERC20, Ownable, ReentrancyGuard {
     address public token;
     bool public isAnchor;
 
@@ -10,11 +12,53 @@ contract ZirconAnchor is IUniswapV2ERC20, Ownable {
     //TODO: function redeem
     constructor() {}
 
-    function mint() onlyOwner {
+    function _mintFee(uint112 _reserve0, uint112 _reserve1) private returns (bool feeOn) {
+        address feeTo = IUniswapV2Factory(factory).feeTo();
+        feeOn = feeTo != address(0);
+        uint _kLast = kLast; // gas savings
+        if (feeOn) {
+            if (_kLast != 0) {
+                uint rootK = Math.sqrt(uint(_reserve0).mul(_reserve1));
+                uint rootKLast = Math.sqrt(_kLast);
+                if (rootK > rootKLast) {
+                    uint numerator = totalSupply.mul(rootK.sub(rootKLast));
+                    uint denominator = rootK.mul(5).add(rootKLast);
+                    uint liquidity = numerator / denominator;
+                    if (liquidity > 0) _mint(feeTo, liquidity);
+                }
+            }
+        } else if (_kLast != 0) {
+            kLast = 0;
+        }
+    }
+
+
+    function mint(uint112 _reserve, uint _balance) onlyOwner nonReentrant {
+        // diff anchor
+        // liquidita aggiunta in float con liquidita
+        // TPV * gamma
+        // balance
+
+        uint amount = balance.sub(_reserve);
+
+        bool feeOn = _mintFee(_reserve0, _reserve1);
+        uint _totalSupply = totalSupply; // gas savings, must be defined here since totalSupply can update in _mintFee
+        if (_totalSupply == 0) {
+            liquidity = Math.sqrt(amount0.mul(amount1)).sub(MINIMUM_LIQUIDITY);
+            _mint(address(0), MINIMUM_LIQUIDITY); // permanently lock the first MINIMUM_LIQUIDITY tokens
+        } else {
+            liquidity = Math.min(amount0.mul(_totalSupply) / _reserve0, amount1.mul(_totalSupply) / _reserve1);
+        }
+        require(liquidity > 0, 'UniswapV2: INSUFFICIENT_LIQUIDITY_MINTED');
+        _mint(to, liquidity);
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        if (feeOn) kLast = uint(reserve0).mul(reserve1); // reserve0 and reserve1 are up-to-date
+        emit Mint(msg.sender, amount0, amount1);
 
     }
 
-    function redeem() onlyOwner {
+    function redeem() onlyOwner lock {
 
     }
 }
