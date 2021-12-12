9712b80dcbcc9eb2a31c9a38c45e82d761829934
David Vittori <me@vittoridavid.com>
2021-12-12 18:04:02 +0100

    ZA-9 Router boilerplate

--- contracts/ZirconRouter.sol
+++ contracts/ZirconRouter.sol
@@ -1,14 +1,69 @@
-pragma solidity ^0.4.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.2;
+import "./zircon-uniswapv2/interfaces/IUniswapV2Router02.sol";
 
+contract ZirconRouter is IUniswapV2Router02 {
 
-// Zircon Periferical
+    function addSyncLiquidity(
+        address anchorToken,
+        address floatToken,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        (amountA, amountB) = _addLiquidity(anchorToken, floatToken, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = UniswapV2Library.pairFor(factory, anchorToken, floatToken);
+        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        liquidity = IUniswapV2Pair(pair).mint(to);
+    }
 
-// TODO: Extends UIv2 Router
+    function addAsyncLiquidity(
+        address anchorToken,
+        address floatToken,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        (amountA, amountB) = _addLiquidity(anchorToken, floatToken, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = UniswapV2Library.pairFor(factory, anchorToken, floatToken);
+        TransferHelper.safeTransferFrom(anchorToken, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(floatToken, msg.sender, pair, amountB);
+        liquidity = IUniswapV2Pair(pair).mint(to);
+    }
 
-contract ZirconRouter {
+    function removeSyncLiquidity(
+        address token,
+        address tokenB,
+        bool isAnchor,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountETH) {
 
-    //TODO: add/remove float/anchor liquidity
+    }
+
+    function removeAsyncLiquidity(
+        address token,
+        address tokenB,
+        bool isAnchor,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountETH) {
+
+    }
+
     //TODO: Supply/remove anchor/float synchronous/asynchronous
-
     function ZirconRouter(){}
 }
